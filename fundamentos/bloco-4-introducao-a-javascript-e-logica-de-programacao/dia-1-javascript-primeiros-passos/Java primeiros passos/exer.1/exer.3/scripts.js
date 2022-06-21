c0f433be80cb03d26350ae8f3e162b1916da7e50
Cristiane Kizelevicius <cristiane@samaan.com.br>
2022-06-21 20:58:58 -0300

    fazendo exercicios de 1 a 4 do primeiros passos java

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/Java primeiros passos/exer.1/exer.3/scripts.js
@@ -0,0 +1,13 @@
+const a = 5;
+const b = 3;
+const c = 1;
+
+if (a > b && a > c) {
+  console.log('O maior número é: ' + a + ' (a)');   
+}
+
+if (b > a && b > c) {
+  console.log('O maior número é: ' + b + ' (b)');
+} else { 
+console.log('O maior número é : ' + c + '(c)');
+}
---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/Java primeiros passos/exer.4/scripts.js
@@ -0,0 +1,8 @@
+const number = -1
+if (number > 0) {
+    console.log('positive');
+} else if (number < 0) {
+    console.log('negativo');
+} else {
+    console.log('zero')
+}
