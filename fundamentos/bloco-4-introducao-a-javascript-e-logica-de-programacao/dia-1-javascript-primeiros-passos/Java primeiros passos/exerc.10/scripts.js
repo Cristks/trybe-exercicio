60021686c737ebb4fc7932f195023ce2aadce3f5
Cristiane Kizelevicius <cristiane@samaan.com.br>
2022-06-21 23:00:26 -0300

    efetuando exercicios de 08 a 11o

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/Java primeiros passos/exerc.10/scripts.js
@@ -0,0 +1,12 @@
+const custoProduto = 10;
+const  valorVenda = 30;
+
+
+
+if (custoProduto >= 0 && valorVenda >= 0) {
+    custoProdutotal = custoProduto * 20/100;
+    const Lucro = (valorVenda - custoProdutotal) * 1000;
+    console.log(Lucro)
+} else {
+    console.log("Error, os valores não podem ser negativos");
+}
